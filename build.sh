e277c48c77b3ea24a1b887ee51422b0af9d9e7cb
Matthew Schmohl <matthew.schmohl@gmail.com>
2019-04-30 16:59:22 -0400

    fix build scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ./gradlew clean build dist 
+mkdir ./target
 tar -xf frontend/build/distributions/playBinary.tar -C ./target
 tar -xf backend/build/distributions/backend-1.0.tar -C ./target
--- run_frontend.sh
+++ run_frontend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export JAVA_OPTS="-javaagent:/Users/matthewschmohl/dd-grpc-play-demo/dd-java-agent.jar -Ddd.integrations.enabled=true -Ddd.service.name=reffrontend -Ddd.logs.injection=true"
+export JAVA_OPTS="-javaagent:$(pwd)/dd-java-agent.jar -Ddd.integrations.enabled=true -Ddd.service.name=reffrontend -Ddd.logs.injection=true"
 ./target/playBinary/bin/playBinary
