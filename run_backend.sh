834e79a7a05d84e7794f431de60d35cad897876a
Matthew Schmohl <matthew.schmohl@gmail.com>
2019-04-30 16:27:42 -0400

    add java opts

--- run_backend.sh
+++ run_backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-JAVA_OPTS="-javaagent:$pwd/dd-java-agent.jar"
+export JAVA_OPTS="-javaagent:/Users/matthewschmohl/dd-grpc-play-demo/dd-java-agent.jar -Ddd.integrations.enabled=true -Ddd.service.name=refbackend -Ddd.logs.injection=true"
 ./target/backend-1.0/bin/backend
